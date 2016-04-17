6042858be43e53c5c502b158b9a3c3cdf2450e87
James Friend <james@jsdf.co>
2016-04-17 23:28:14 +1000

    updated coffee-react-transform to v4.0.0

--- update.sh
+++ update.sh
@@ -5,14 +5,14 @@ DEPENDENCY="coffee-react-transform"
 VERSION=`npm view ${DEPENDENCY} version`
 echo "updating to $VERSION"
 npm install --save "${DEPENDENCY}@${VERSION}"
-EXECJS_RUNTIME=Node rake test_runtime
+EXECJS_RUNTIME=Node bundle exec rake test_runtime
 git commit -a -m "updated ${DEPENDENCY} to v${VERSION}"
 npm version $VERSION
 read -p "will publish $VERSION. are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  rake release
+  bundle exec rake release
   git push origin master
   git push origin --tags
 fi
